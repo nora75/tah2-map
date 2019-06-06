882b3e6584cb43992cdec7a7237a113fca79e4cd
Wataru Tsutsumi <k017c1055@it-neec.jp>
2019-06-06 11:23:51 +0900

    [change] レイアウトの変更
    ウィジェットを別に宣言して呼び出す形式にした
    まだ機能は特になし

--- lib/Api.dart
+++ lib/Api.dart
@@ -20,6 +20,7 @@ class _ApiState extends State<Api> {
   Widget build(BuildContext context) {
 
     double width = MediaQuery.of(context).size.width;   // 画面の横幅取得
+    double height = MediaQuery.of(context).size.height;   // 画面の縦幅取得
 
     return Scaffold(
 
@@ -28,22 +29,61 @@ class _ApiState extends State<Api> {
         title: Text(title),
       ),
 
-      body: Center(
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
+      body: Card(
+        child: Column(
           children: <Widget>[
-            Container(
-              width: width * 0.5,   // 横幅を画面の50%
-              
-              child: TextFormField(
-                textAlign: TextAlign.center,
-                maxLength: 20,
-              ),
-            ),
-            
+            _SearchBar(width),
+            _PrintText(width, height),
           ],
         ),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+}
+
+Widget _SearchBar(width) {
+  return Row(
+
+  mainAxisAlignment: MainAxisAlignment.center,
+        
+  children: <Widget>[
+    Container(
+      width: width * 0.5,   // 横幅を画面の50%
+      
+      child: TextFormField(
+        textAlign: TextAlign.center,
+        maxLength: 20,
+      ),
+      
+    ),
+    FlatButton(
+      color: Colors.lightBlue[50],
+      onPressed: () {
+        // print("btn");
+      },
+      child: Text("検索"),
+    )
+  ],
+);
+}
+
+Widget _PrintText(width, height) {
+  return Row(
+    mainAxisAlignment: MainAxisAlignment.center,       
+    children: <Widget>[
+      Container(
+        color: Colors.grey[300],
+        width: width * 0.6,
+        height: height * 0.7,
+        child: Text(
+          "TEST",
+          style: TextStyle(
+            fontSize: 50,
+            color: Colors.grey[800],
+          ),
+        ),
+        alignment: Alignment(0.0, 0.0),
+      )
+  ],
+);
 }
