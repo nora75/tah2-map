88582f53a724465b86a3b6122b51aa1196e45dc7
AS-YDK <k017c1052@it-neec.jp>
2019-06-20 10:28:02 +0900

    Changed Tab inside

--- lib/TabPage.dart
+++ lib/TabPage.dart
@@ -10,56 +10,95 @@ class TabPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    double width = MediaQuery.of(context).size.width;   // 画面の横幅取得
+    double height = MediaQuery.of(context).size.height;   // 画面の縦幅取得
+
     return DefaultTabController(
       length: 2,
       child: Scaffold(
         appBar: AppBar(
-          title: Text(title),
+          title: Text("Porn Hub"),
 
           bottom: TabBar(
-            tabs: <Widget>[
+            tabs: <Widget>[   //タブ名
               Tab(text:"Location"),
               Tab(text:"History"),
             ],
           )
         ),
 
-        body: TabBarView(
+        body: TabBarView(   //タブ
           children: <Widget>[
-            Container(
+            Container(    //タブ内容
               color: Colors.red,
-              /*child: Row(
+              padding: EdgeInsets.only(top: 30.0),
+              child: Column(   //横並び
                 children: <Widget>[
-                  RaisedButton(
-                    child: Text("Location"),
-                    onPressed: (){}
-                  ),
-
-                  TextField(
-                    decoration: InputDecoration(
-                      hintText: "Address"
-                    ),
-                  ),
-
-                  Text(
-                    "This is text"
-                  ),
-
-                  RaisedButton(
-                    child: Text("Search"),
-                    onPressed: () {}
-                  ),
+                  _PrintText(width, height),
+                  _SearchBar(width)
                 ]
-              ),*/
+              ),
             ),
             
             Container(
               color: Colors.yellow,
+              // child: Column(   //横並び
+              //   children: <Widget>[
+                  
+              //   ]
+              // )
             )
           ],
         ),
       )
     );
   }
+}
+
+Widget _SearchBar(width) {
+  return Row(
+
+  mainAxisAlignment: MainAxisAlignment.center,
+        
+  children: <Widget>[
+    Container(
+      width: width * 0.7,   // 横幅を画面の50%
+      
+      child: TextFormField(
+        textAlign: TextAlign.center,
+        maxLength: 20,
+      ),
+      
+    ),
+    FlatButton(
+      color: Colors.lightBlue[50],
+      onPressed: () {
+        // print("btn");
+      },
+      child: Text("検索"),
+    )
+  ],
+);
+}
+
+Widget _PrintText(width, height) {
+  return Row(
+    mainAxisAlignment: MainAxisAlignment.center,       
+    children: <Widget>[
+      Container(
+        color: Colors.grey[300],
+        width: width * 0.7,
+        height: height * 0.6,
+        child: Text(
+          "TEST",
+          style: TextStyle(
+            fontSize: 50,
+            color: Colors.grey[800],
+          ),
+        ),
+        alignment: Alignment(0.0, 0.0),
+      )
+  ],
+);
 }
 
