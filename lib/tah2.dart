631af0ceac4ff6e83398b8f6a876b30b09304b49
Wataru Tsutsumi <k017c1055@it-neec.jp>
2019-09-12 03:39:33 +0900

    [new] APIをたたけるように
    テキストフィールドへの入力ができず、簡単なチェックのみ
    結果はListに入る
    コンビニフィルターをクリック時、フラグを立てれるように

---
+++ lib/tah2.dart
@@ -0,0 +1,179 @@
+import 'dart:html';
+import 'package:flutter_web/material.dart';
+
+import 'dart:async';
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
+class tah2 extends StatefulWidget {
+  tah2() : super();
+
+  @override
+  _tah2State createState() => _tah2State();
+}
+
+class _tah2State extends State<tah2> {
+
+  List<String> s = ["a", "b"];
+  final _controller = TextEditingController(); // テキストの内容の取得や設定に使用するコントローラ
+  bool konbini = false;
+
+
+  Future apiRequest(data) async {
+    if(konbini) {
+      data = data + " コンビニ";
+    }
+    data = data.toString().replaceAll("　", " ");     // 全角スペースの削除
+    // String results = "";
+    // http.Response response = await http.get("https://script.google.com/macros/s/AKfycbxvArbjHqRgX1_lI0L2an9Nvkzv0n-Gfqyu95u0wmHkgB3AWueQ/exec?word=" + data);
+    // Map<String, dynamic> decoded = await json.decode(response.body);
+    // for(var result in decoded['results']){
+    //   results += "名称 : ";
+    //   results += result['name'];
+    //   // results += "\n\t住所 : ";
+    //   // results += result['formatted_address'];
+    //   results += "\n";
+    // }
+
+    List<String> results = [];
+    s.clear();
+    http.Response response = await http.get("https://script.google.com/macros/s/AKfycbxvArbjHqRgX1_lI0L2an9Nvkzv0n-Gfqyu95u0wmHkgB3AWueQ/exec?word=" + data);
+    Map<String, dynamic> decoded = await json.decode(response.body);
+    for(var result in decoded['results']){
+      results.add(result['name']);
+      print(result['name']);
+    }
+    setState(() {
+      s = results;
+    });
+  }
+   
+  final String title = "tah2";
+  final GlobalKey<ScaffoldState> scaffoldKey = GlobalKey<ScaffoldState>();
+
+
+  @override
+
+  Widget build(BuildContext context) {
+    double width = MediaQuery.of(context).size.width; // 画面の横幅取得
+    double height = MediaQuery.of(context).size.height; // 画面の縦幅取得
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Tah2-Map"),
+        backgroundColor: Colors.blue[500],
+      ),
+      body: Container(
+        padding: EdgeInsets.only(top: 0.0, bottom: 0.0),
+        child: Stack(children: <Widget>[
+          Container(
+            child: _PrintText(width, height),
+          ),
+          Container(
+              padding: EdgeInsets.only(top : height * 0.80),
+              child: _SearchBar(width, context))
+        ])),
+      drawer: Drawer(
+        elevation: 20.0,
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            DrawerHeader(
+              child: Text('フィルタリング'),
+              decoration: BoxDecoration(color: Colors.blueAccent),
+            ),
+            ListTile(
+              title: Text('コンビニ'),
+              onTap: () {
+                konbini = true;
+                print("コンビニ");
+              },
+            ),
+            ListTile(
+              title: Text('スーパー'),
+              onTap: () {},
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _PrintText(width, height) {
+      return Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Container(
+              //color: Colors.blue[100],
+              // width: width, // 横幅全部
+              // height: height, // 縦幅65% 70%はスマホレイアウト狂う
+              // child:
+              // Text(
+              //   "TEST",
+              //   style: TextStyle(
+              //     fontSize: 50,
+              //     color: Colors.pink[300],
+              //   ),
+              // ),
+              //alignment: Alignment(0.0, 0.0),
+              )
+        ],
+      );
+    }
+  
+  Widget _SearchBar(width, context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Container(
+          width: width * 0.7, // 横幅を画面の70%
+
+          decoration: BoxDecoration(
+            color: Colors.white,
+            border: Border.all(
+              width: 2.0, // 線の太さ
+              color: Colors.lightBlue[500],
+            ),
+            borderRadius: BorderRadius.all(Radius.circular(5.0) // かどまる君
+                ),
+          ),
+
+          child: TextField(
+            decoration: InputDecoration.collapsed(
+              border: InputBorder.none,
+              hintText: "キーワードまたは座標を入力",
+            ),
+            inputFormatters: [
+              LengthLimitingTextInputFormatter(100), // 文字数の入力制限(カウンターを表示しない)
+            ],
+            textAlign: TextAlign.center,
+            controller: _controller,
+            style: TextStyle(fontSize: 20.0, color: Colors.red)
+          ),
+        ),
+        ButtonTheme(
+            child: FlatButton(
+          color: Colors.lightBlue[500],
+          onPressed: () {
+            if(_controller.text.isEmpty) {
+              return showDialog(
+                context: context,
+                builder: (context) {
+                  return AlertDialog(
+                    content: Text('文字を入力してください')
+                  );
+                },
+              );
+            }
+            else{
+              apiRequest(_controller.text);
+              // apiRequest('蒲田');
+            }
+          },
+          child: Text("検索"),
+        ))
+      ],
+    );
+  }
+}
