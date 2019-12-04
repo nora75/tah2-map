b19093ff0d31b9440b5e948c6304af580b0a5edc
Wataru Tsutsumi <k017c1055@it-neec.jp>
2019-12-04 16:01:49 +0900

    [chg] ApiListをステートフルウィジェットに変更
    ステートレスウィジェットでは動的に更新できないため

--- lib/ApiList.dart
+++ lib/ApiList.dart
@@ -2,7 +2,20 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'Api.dart';
 
-class ApiList extends StatelessWidget {
+class ApiList extends StatefulWidget {
+  ApiList({Key key}): super(key: key);
+
+  @override
+  _ApiList createState() => _ApiList();
+}
+
+class _ApiList extends State<ApiList> {
+  final _controller = TextEditingController(); // テキストの内容の取得や設定に使用するコントローラ
+
+  var result = [];    // 検索結果
+
+  // var n = 0;
+
   @override
   Widget build(BuildContext context) {
     double width = MediaQuery.of(context).size.width; // 画面の横幅取得
@@ -16,7 +29,24 @@ class ApiList extends StatelessWidget {
         ),
         Container(
           child: SearchBar(width, context)
-        )
+        ),
+        // Container(
+        // child: Text(n.toString()),
+        // ),
+        // Container(
+        //   margin: EdgeInsets.only(top: 50),
+        //   child: RaisedButton(
+        //     child: Text("btn"),
+        //     color: Colors.red,
+        //     onPressed: (){
+        //       setState((){
+        //         n = n + 1;
+        //         print("$n");
+        //       }
+        //       );
+        //     },
+        //   )
+        // ),
       ])
     );
   }
